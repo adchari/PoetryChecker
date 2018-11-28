cefcbd15c0a15c6a2e33f7791946f0c4f34072c0
Adithya Chari <adithya.chari@gmail.com>
2018-11-28 16:35:01 -0600

    ReadMe updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-PoetryChecker
+# Poetry Checker
+
+### To-Do
+
+- [ ] Find out how to get data from API
+- [ ] Make frontend
+- [ ] Interact with data
+
--- app/src/main/java/com/example/adith/poetrychecker/MainActivity.java
+++ app/src/main/java/com/example/adith/poetrychecker/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.adith.poetrychecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txt = (TextView) findViewById(R.id.txtid);
+        try {
+            URL url = new URL("https://api.datamuse.com/words?sl=tomatoes");
+            URLConnection con = url.openConnection();
+            InputStream in = con.getInputStream();
+            String encoding = con.getContentEncoding();
+            encoding = encoding == null ? "UTF-8" : encoding;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buf = new byte[8192];
+            int len = 0;
+            while ((len = in.read(buf)) != -1) {
+                baos.write(buf, 0, len);
+            }
+            String body = new String(baos.toByteArray(), encoding);
+            txt.setText(body);
+        } catch (Exception e) {
+            Log.e("HELP", e.getMessage(), e);
+        }
     }
 }
