8e373855bc4af54f60013a75c9a880d4d6217237
Adithya Chari <adithya.chari@gmail.com>
2018-11-29 11:21:53 -0600

    Data can be pulled

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 ### To-Do
 
-- [ ] Find out how to get data from API
+- [x] Find out how to get data from API
 - [ ] Make frontend
 - [ ] Interact with data
+
+### Details
+
+The main API we pull from is [DataMuse](https://www.datamuse.com/api/). This API allows us to get details on words, including rhymes and syllable counts.
+
+This application is designed to work on Android devices and is written in Java.
+
+### Latest Commits
+
+##### Data can be pulled
+
+- JSON information from DataMuse can be pulled into the Android application as a String. This is not formatted as JSON.
+- This was accomplished using additional threads to work on network functions in the background.
+- Currently, this hardcoded request appends to a label.
+
+​	
 
--- app/src/main/java/com/example/adith/poetrychecker/MainActivity.java
+++ app/src/main/java/com/example/adith/poetrychecker/MainActivity.java
@@ -2,43 +2,48 @@ package com.example.adith.poetrychecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txt = (TextView) findViewById(R.id.txtid);
-        try {
-            URL url = new URL("https://api.datamuse.com/words?sl=tomatoes");
-            URLConnection con = url.openConnection();
-            InputStream in = con.getInputStream();
-            String encoding = con.getContentEncoding();
-            encoding = encoding == null ? "UTF-8" : encoding;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            byte[] buf = new byte[8192];
-            int len = 0;
-            while ((len = in.read(buf)) != -1) {
-                baos.write(buf, 0, len);
+    }
+    public void btnOnClick(View v) {
+        final TextView txt = findViewById(R.id.txtid);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final String toSet = getData("https://api.datamuse.com/words?sl=periwinkle");
+                    txt.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            txt.append(toSet);
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            String body = new String(baos.toByteArray(), encoding);
-            txt.setText(body);
-        } catch (Exception e) {
-            Log.e("HELP", e.getMessage(), e);
+        }).start();
+    }
+    public static String getData(String url) throws Exception {
+        URL api = new URL(url);
+        HttpURLConnection link = (HttpURLConnection) api.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
+        in.close();
+        return response.toString();
     }
 }
